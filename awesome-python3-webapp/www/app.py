574995666b81d8b382bef2d8d3de98d9af4edab6
Solotzy <498442999@qq.com>
2016-10-20 12:51:58 +0800

    commit

--- awesome-python3-webapp/www/app.py
+++ awesome-python3-webapp/www/app.py
@@ -1,7 +1,20 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import logging; logging.basicConfig(level=logging.INFO)
 
+################################################################################
+#
+# Copyright (c) 2016 Jurassic.com, Inc. All Rights Reserved
+#
+################################################################################
+"""
+This module provide configure file management service in i18n environment.
+
+Authors: tianzeyu(tianzeyu@jurassic.com.cn)
+Date:    2016/10/19 18:13:23
+"""
+
+import logging; logging.basicConfig(level=logging.INFO)
 import asyncio, os, json, time
 from datetime import datetime
 from aiohttp import web
