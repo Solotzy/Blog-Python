c87a6564ade2fde6fc1c4df75bfc7d1ebeaa691b
Solotzy <498442999@qq.com>
2016-10-20 14:39:01 +0800

    import

--- awesome-python3-webapp/test/test.py
+++ awesome-python3-webapp/test/test.py
@@ -1,11 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+################################################################################
+#
+# Copyright (c) 2016 Jurassic.com, Inc. All Rights Reserved
+#
+################################################################################
+"""
+This module provide configure file management service in i18n environment.
+
+Authors: tianzeyu(tianzeyu@jurassic.com.cn)
+Date:    2016/10/20 13:23:06
+"""
+
 import orm
 import asyncio
 import sys
-from models import User, Blog, Comment
+import models
+#from models import User, Blog, Comment
 
 async def test(loop):
     await orm.create_pool(loop=loop, user='www-data', password='www-data', db='awesome')
-    u = User(name='Test5', email='test5@example.com', passwd='123456789032', image='about:blank')
+    u = models.User(name='Test5', email='test5@example.com', passwd='123456789032', image='about:blank')
     print(u)
     await u.save()
     print('tested ok...')
